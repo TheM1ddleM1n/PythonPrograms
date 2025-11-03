82dd53362e0b31578045a7ad239dea8c115d96e7
TheM1ddleM1n <192749026+TheM1ddleM1n@users.noreply.github.com>
2025-11-03 22:23:20 +0000

    Update loops.py
    was a couple of duplicates

--- Other Programs/loops.py
+++ Other Programs/loops.py
@@ -1,4 +1,4 @@
-fruits = ["apple", "banana", "cherry", "Apricot", "Avocado", "Banana", "Blueberry", "Cherry", "Coconut", "Cranberry", "Date", "Fig", "Grapes", "Grapefruit", "Guava", "Kiwi", "Lemon", "Lime", "Mango", "Orange", "Papaya", "Peach", "Pear", "Pineapple", "Plum", "Pomegranate", "Raspberry", "Strawberry", "Watermelon."]
+fruits = ["Apple", "Banana", "Apricot", "Avocado", "Blueberry", "Cherry", "Coconut", "Cranberry", "Date", "Fig", "Grapes", "Grapefruit", "Guava", "Kiwi", "Lemon", "Lime", "Mango", "Orange", "Papaya", "Peach", "Pear", "Pineapple", "Plum", "Pomegranate", "Raspberry", "Strawberry", "Watermelon"]
 
 for fruit in fruits:
     print(fruit)
