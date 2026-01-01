5d784aa2f25f6fbe484ff1fcaabd02d6aaa99027
TheMiddleMan <192749026+TheM1ddleM1n@users.noreply.github.com>
2026-01-01 21:08:39 +0000

    Update heads vs tails.py
    fixed

--- Number/heads vs tails.py
+++ Number/heads vs tails.py
@@ -6,13 +6,12 @@ turns = 0
 
 print('Welcome to the heads and tails coin flipping machine')
 
-while turns !=100:
-   flips = random.randint(1,2)
-   if flips== 1:
-       heads = heads+1
-else:
-   tails = tails+1
+while turns != 100:
+    flips = random.randint(1, 2)
+    if flips == 1:
+        heads = heads + 1
+    else:
+        tails = tails + 1
+    turns = heads + tails
 
-turns = heads + tails 
-
-print('The number of heads flipped was',heads,'the number of tails flipped was', tails)
+print('The number of heads flipped was', heads, 'the number of tails flipped was', tails)
