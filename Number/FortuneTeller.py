5ab2126a16d7a3be60930230c2edf9e8af587fd5
TheMiddleMan <192749026+TheM1ddleM1n@users.noreply.github.com>
2026-01-01 21:10:04 +0000

    Update FortuneTeller.py

--- Number/FortuneTeller.py
+++ Number/FortuneTeller.py
@@ -3,39 +3,33 @@ import random
 fortune = 0
 replay = "y"
 
-
-print('Welcome to ModuleMaster64 fortune telling program!')
-replay =int('Do you want to have your fortune read? Enter y to play on or press enter to exit.')
+print('Welcome to TheM1ddleM1n\'s fortune telling program!')
+replay = input('Do you want to have your fortune read? Enter y to play on or press enter to exit: ')
 
 while replay == "y":
+    fortune = random.randint(1, 5)
 
-    fortune = random.randint(1,5)
-
-    if fortune ==1:
+    if fortune == 1:
         print('This is your lucky day, you are going to win the lottery!')
-        replay = input('Do you want to play again? Enter y to play again or press enter to exit')
+        replay = input('Do you want to play again? Enter y to play again or press enter to exit: ')
         print()
 
-    if fortune ==2:
+    elif fortune == 2:
         print('It is going to rain all holiday! What do you do now??')
-        replay = input('Do you want to play again? Enter y to play again or press enter to exit')
+        replay = input('Do you want to play again? Enter y to play again or press enter to exit: ')
         print()
 
-    if fortune ==3:
+    elif fortune == 3:
         print('Congratulations! You bought a flashy car with you lottery money!')
-        replay = input('Do you want to play again? Enter y to play again or press enter to exit')
+        replay = input('Do you want to play again? Enter y to play again or press enter to exit: ')
         print()
 
-    if fortune ==4:
+    elif fortune == 4:
         print('Congratulations! You also bought a mansion with your money. Very nice :)')
-        replay = input('Do you want to play again? Enter y to play again or press enter to exit')
+        replay = input('Do you want to play again? Enter y to play again or press enter to exit: ')
         print()
 
-    elif fortune ==5:
+    elif fortune == 5:
         print('Oh no! You are bankrupt you spent too much money. What do you do now??')
-        replay = input('Do you want to play again? Enter y to play again or press enter to exit')
+        replay = input('Do you want to play again? Enter y to play again or press enter to exit: ')
         print()
-        
-
-        
-    
