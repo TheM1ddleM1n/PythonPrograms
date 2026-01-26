88756a7537770f2639db7a3dc7de29c26065fc04
TheM1ddleM1n <192749026+TheM1ddleM1n@users.noreply.github.com>
2026-01-26 21:39:56 +0000

    Delete Other Programs/Story.py
    not needed ig

--- Other Programs/Story.py
+++
@@ -1,9 +0,0 @@
-# String data types
-
-# Subroutine to process a phrase!
-def Output(Phrase):
-    print(Phrase[20:26])
-
-# Main program
-Phrase = "I saw a wolf in the forest. A lonely wolf."
-Output(Phrase)
