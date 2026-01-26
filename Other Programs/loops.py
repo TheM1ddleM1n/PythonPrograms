a998c59190d0e30a9902d4b918f446a567957571
TheM1ddleM1n <192749026+TheM1ddleM1n@users.noreply.github.com>
2026-01-26 21:39:43 +0000

    Delete Other Programs/loops.py

--- Other Programs/loops.py
+++
@@ -1,4 +0,0 @@
-fruits = ["Apple", "Banana", "Apricot", "Avocado", "Blueberry", "Cherry", "Coconut", "Cranberry", "Date", "Fig", "Grapes", "Grapefruit", "Guava", "Kiwi", "Lemon", "Lime", "Mango", "Orange", "Papaya", "Peach", "Pear", "Pineapple", "Plum", "Pomegranate", "Raspberry", "Strawberry", "Watermelon"]
-
-for fruit in fruits:
-    print(fruit)
