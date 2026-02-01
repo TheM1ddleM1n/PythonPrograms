fa3e976057ed7bc02daa19d042628b04c1971aa0
TheM1ddleM1n <192749026+TheM1ddleM1n@users.noreply.github.com>
2026-02-01 14:08:44 +0000

    Update Lucky.py

--- Number/Lucky.py
+++ Number/Lucky.py
@@ -1,7 +1,13 @@
-print('Welcome to the Lucky program!')
-a=int(input('Please enter the month number e.g. November is 11'))
-b=int(input('Please enter the last two numbers of your year e.g.01'))
-c=int(input('Please enter the first number of the day of your birthday\n for example if your birthday is on the 16th day of the month,enter 1'))
-Lucky=a+b+c
+"""Lucky number calculator based on birth details."""
 
-print('Your lucky number is ' +str(Lucky))
+print('Welcome to the Lucky program!')
+try:
+    month = int(input('Please enter the month number e.g. November is 11: '))
+    year = int(input('Please enter the last two numbers of your year e.g. 01: '))
+    day = int(input('Please enter the first number of the day of your birthday\n for example if your birthday is on the 16th day of the month, enter 1: '))
+    
+    lucky_number = month + year + day
+    print(f'Your lucky number is {lucky_number}')
+    
+except ValueError:
+    print("❌ Error: Please enter valid numbers only.")
