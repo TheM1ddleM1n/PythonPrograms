ad13d67b0cc0a92465915e763cd53782fa7307fc
TheM1ddleM1n <192749026+TheM1ddleM1n@users.noreply.github.com>
2026-05-20 20:10:38 +0100

    Update MyFirstProgram.py

--- Other Programs/MyFirstProgram.py
+++ Other Programs/MyFirstProgram.py
@@ -1,10 +1,5 @@
-# First Python program
-
-
 def output_text():
-    """Prints a cheeky message about Python."""
     print("Python’s proper brilliant innit?")
-
 
 if __name__ == "__main__":
     output_text()
