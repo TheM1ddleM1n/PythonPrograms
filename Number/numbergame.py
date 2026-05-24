1c5930bc53eb877c396bbcb7b456d389d33393de
TheM1ddleM1n <192749026+TheM1ddleM1n@users.noreply.github.com>
2026-05-24 16:56:03 +0100

    'try' this lol

--- Number/numbergame.py
+++ Number/numbergame.py
@@ -1,21 +1,18 @@
-number = 777
+SECRET = 777
 
 print("In this program you will need to guess the number I am thinking of")
-
 print("I am thinking of a number between 1 and 800")
 
-while number == 777:
-    guess = input()
+while True:
+    try:
+        guess = int(input())
 
-    guess = int(guess)
-
-    if guess < 777:
-        print("Your guess is too low! Please try again.")
-
-    if guess > 777:
-        print("Your guess is too high! Please try again.")
-
-    if guess == 777:
-        print("Well done!! Your guess is correct!")
-
-        break
+        if guess < SECRET:
+            print("Your guess is too low! Please try again.")
+        elif guess > SECRET:
+            print("Your guess is too high! Please try again.")
+        else:
+            print("Well done!! Your guess is correct!")
+            break
+    except ValueError:
+        print("Please enter a valid number.")
