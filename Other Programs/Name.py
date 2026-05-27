aea32091cf842c8eb26283bd39a947c84860ff54
TheM1ddleM1n <192749026+TheM1ddleM1n@users.noreply.github.com>
2026-05-27 20:02:49 +0100

    Update Name.py

--- Other Programs/Name.py
+++ Other Programs/Name.py
@@ -1,16 +1,13 @@
 import re
-
-
 def is_valid_name(name):
     return re.fullmatch(r"[A-Za-z\s\-']+", name) is not None
-
 
 surname = input("Enter your surname: ")
 forename = input("Enter your forename: ")
 
 if not (is_valid_name(surname) and is_valid_name(forename)):
     print(
-        "\n⚠️ This was a invalid input. Please use only letters, spaces, hyphens, or apostrophes!"
+        "\nThis is an invalid input. Please use only letters, spaces, hyphens, or apostrophes please!"
     )
 else:
     full_name = f"{forename} {surname}"
