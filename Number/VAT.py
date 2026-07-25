94eca94b5571643a51dd5088a1c0030a1adc6f3f
TheM1ddleM1n <192749026+TheM1ddleM1n@users.noreply.github.com>
2026-07-25 14:33:46 +0100

    remove comments v2 and update VAT

--- Number/VAT.py
+++ Number/VAT.py
@@ -1,12 +1,7 @@
-# Number data types
-
-# Subroutine to calculate VAT
 def VAT(Total):
     return Total * 0.05
 
-
-# Main program
 Total = 100.12
 ValueAddedTax = VAT(Total)
 ToPay = Total + ValueAddedTax
-print("Total £{:.2f} VAT £{:.2f} To pay £{:.2f}".format(Total, ValueAddedTax, ToPay))
+print(f"Total £{Total:.2f} VAT £{ValueAddedTax:.2f} To pay £{ToPay:.2f}")
